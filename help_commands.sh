ef885c9babb473c5c142de12adeb2c1eee9a23a6
Statys <vsastatys@gmail.com>
2023-03-07 02:39:35 +0400

    test autobuild

--- help_commands.sh
+++ help_commands.sh
@@ -5,5 +5,8 @@ statys/openai_bot:0.1
 
 docker run -d \
   --name watchtower \
+  -e WATCHTOWER_POLL_INTERVAL=10 \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  containrrr/watchtower
+  containrrr/watchtower statys/openai_bot:0.1 --debug
+
+watch -n 1 docker container ps
