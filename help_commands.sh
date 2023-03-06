07cd2f261231224a76baf44e16727b7b0649113f
Statys <vsastatys@gmail.com>
2023-03-07 02:10:37 +0400

    test autobuild

--- help_commands.sh
+++ help_commands.sh
@@ -1,2 +1,9 @@
 docker compose -f compose up --detach
 docker compose -f compose down
+#check_updates
+statys/openai_bot:0.1
+
+docker run -d \
+  --name watchtower \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  containrrr/watchtower
